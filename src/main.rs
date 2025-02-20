56efe3c907eb7d494892ce5d8fec6bf29e495358
Fawzi Noual <fouzi.noual@gmail.com>
2025-02-20 22:22:59 +0100

    Added logger

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,12 @@ mod handlers;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    env_logger::init();
+
     HttpServer::new(|| {
         App::new()
-            .wrap(actix_middleware::Logger::default()) // Utilisation du renommage
+            // .wrap(actix_middleware::Logger::default()) // Utilisation du renommage
+            .wrap(actix_middleware::Logger::new("%a \"%r\" %s %b \"%{Referer}i\" \"%{User-Agent}i\" %T"))
             .configure(routes::config)
             .default_service(web::route().to(middleware::error_handler))
     })
